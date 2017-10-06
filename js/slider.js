9f7024b6743c354a6149e117fdd997e8416ea72d
paulmatafox <freelancermt.fox@gmail.com>
2017-10-06 18:05:38 +0300

    first

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.pm-slider').slick({
+        'autoplay': true,
+        'slidesToShow': 3,
+        'slidesToScroll': 1,
+        'autoplaySpeed': 2000,
+        'speed': 2000,
+        'prevArrow': '<a href="" class="pm-slider__button pm-slider__button--prev"></a>',
+        'nextArrow': '<a href="" class="pm-slider__button pm-slider__button--next"></a>'
+    });
+});
